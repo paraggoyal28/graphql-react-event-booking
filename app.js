d07dbf4397b61b956910aad95e00ef211d38dd88
Parag Kumar Goyal <1997parag.goyal@gmail.com>
2025-01-12 20:40:25 +0530

    initial code for graphql node backend
    add necessary dependencies
    added routes and models
    added schemas, resolvers

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
-const { buildSchema } = require("graphql");
-
+const mongoose = require("mongoose");
 const app = express();
-
-const events = [];
+const graphQlSchema = require("./graphql/schema/index");
+const graphQlResolvers = require("./graphql/resolvers/index");
 
 app.use(bodyParser.json());
 
@@ -16,55 +15,22 @@ app.get("/", (req, res, next) => {
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        input EventInput {
-            title: String!,
-            description: String!,
-            price: Float!,
-            date: String!
-        }
-
-        type RootQuery {
-            events: [Event!]!
-        }
-
-        type RootMutation {
-            createEvent(eventInput: EventInput): Event
-        }
-        
-        schema {
-            query: RootQuery 
-            mutation: RootMutation
-        }    
-    `),
-    rootValue: {
-      events: (args) => {
-        return events;
-      },
-      createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: args.eventInput.date,
-        };
-        events.push(event);
-        return event;
-      },
-    },
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
     graphiql: true,
   })
 );
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@bookappcluster.rgue4.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=BookAppCluster`
+  )
+  .then((res) => {
+    console.log("MongoDB connected successfully!");
+    app.listen(3000, () => {
+      console.log("Listening on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
